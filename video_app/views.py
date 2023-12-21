4f99645026eae953ce1aa9762168612fe34274cf
GalionSorent <miron.esanov2004@gmail.com>
2023-12-21 19:18:55 +0500

    Video_project

--- video_app/views.py
+++ video_app/views.py
@@ -1,11 +1,3 @@
 from django.shortcuts import render
-from .models import Video, Comment
 
-def video_list(request):
-    videos = Video.objects.all()
-    return render(request, 'video_app/video_list.html', {'videos': videos})
-
-def video_detail(request, video_id):
-    video = Video.objects.get(id=video_id)
-    comments = Comment.objects.filter(video=video)
-    return render(request, 'video_app/video_detail.html', {'video': video, 'comments': comments})
+# Create your views here.
